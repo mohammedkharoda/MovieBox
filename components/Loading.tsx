04a334fbecf40deb63ea4749c5d3d2a233f93462
Mohammed Kharoda <kharodawalam@gmail.com>
2024-01-02 22:28:35 +0530

    adding comments

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,9 +5,9 @@ import { helix } from "ldrs";
 
 // Default values shown
 const Loading = () => {
+  // ldrs loader
   helix.register();
   return (
-    // Loader
     <div className="h-full flex items-center justify-center">
       <l-helix size="55" speed="2.5" color="red"></l-helix>
     </div>
